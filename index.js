57f4ae4f8b120daa0f8df3a591ff4abdc356b5eb
tomassw <75063636+tomassw@users.noreply.github.com>
2022-11-07 14:59:20 +0200

    Added the toggle drawing feature

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let color = 'black';
+let click = true;
 
 function populateBoard(size) {
     let board = document.querySelector('.board');
@@ -30,10 +31,12 @@ function changeSize(input) {
 }
 
 function colorSquare() {
-    if (color === 'random') {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    } else {
-        this.style.backgroundColor = color;
+    if (click) {
+        if (color === 'random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = color;
+        }
     }
     
 }
@@ -47,3 +50,14 @@ function resetBoard() {
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.style.backgroundColor = 'white'); 
 }
+
+document.querySelector('.board').addEventListener('click', (e) => {
+    if (e.target.tagName != "button") {
+        click = !click;
+        if(click) { 
+            document.querySelector('.mode').textContent = "Mode: Coloring";
+        } else {
+            document.querySelector('.mode').textContent = "Mode: Not coloring";
+        }
+    }
+})
